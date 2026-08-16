8088302f2adfc547c8f51d6127cc9873ec34bad1
alber240 <bigchicha74@gmail.com>
2026-08-16 20:31:56 +0200

    FIX: Replace JSONB with JSON in models.py import

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, DECIMAL, Boolean, Text, CHAR, JSONB
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey, DECIMAL, Boolean, Text, CHAR, JSON
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from datetime import datetime
